b91bdda705ca910672599146e41565017e091669
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-02-25 10:40:38 +0200

    completed console

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+
+// JAVA BANKING PROGRAM FOR BEGINNERS
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // JAVA BANKING PROGRAM FOR BEGINNERS
+        System.out.println("***************");
+        System.out.println("Banking Program");
+        System.out.println("***************");
+        System.out.println("1. Show Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. exit");
+        System.out.println("***************");
+
+        // DECLARE VARIABLES
+
+        // DISPLAY MENU
+
+        // GET AND PROCESS USERS CHOICE
+
+        // showBalance()
+
+        // deposit()
+
+        // withdraw()
+
+        // EXIT MESSAGE
+
     }
 }
